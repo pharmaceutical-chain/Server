a28b9f136c3cdcb8c29d9568b8f8260099c84148
Lam Le <15520405@gm.uit.edu.vn>
2019-10-23 17:22:11 +0700

    Initialize medicine batch class

---
+++ src/PharmaceuticalChain.API/Models/Database/MedicineBatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmaceuticalChain.API.Models.Database
+{
+    public class MedicineBatch
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string CommercialName { get; set; }
+        public string RegistrationCode { get; set; }
+        public string BatchNumber { get; set; }
+        public bool IsPrescriptionMedicine { get; set; }
+        public string DosageForm { get; set; }
+        public string IngredientConcentration { get; set; }
+        public uint Quantity { get; set; }
+        public uint DeclaredPrice { get; set; }
+    }
+}
