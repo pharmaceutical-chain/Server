bc8c2247de369b327bd2f56cf79147957def9ea8
Lam Le <15520405@gm.uit.edu.vn>
2019-11-04 23:49:52 +0700

    Adding migration

---
+++ src/PharmaceuticalChain.API/Migrations/20191104163634_AddEmailToTenant.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PharmaceuticalChain.API.Migrations
+{
+    public partial class AddEmailToTenant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Tenants",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Tenants");
+        }
+    }
+}
